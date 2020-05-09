77461dc9bf7b57f5c29a11d9d4833f3c8099fbde
HuanJin <1158649104@qq.com>
2020-05-09 15:37:49 +0800

    Initial commit

---
+++ src/com/jinhuan/test/MyTest.java
@@ -0,0 +1,24 @@
+package com.jinhuan.test;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author jinhaun
+ * @date 2020/5/8 0008 下午 11:01
+ * @projectName testApplication
+ */
+public class MyTest {
+    public static void main(String[] args) {
+        Map mao = new HashMap<String, String>();
+        List<String> list = new ArrayList<>();
+//        list.add();
+        if (list == null) {
+            if (list != null) {
+            }
+        }
+        System.out.println();
+    }
+}
---
+++ web/index.jsp
@@ -0,0 +1,17 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Administrator
+  Date: 2020/5/8 0008
+  Time: 下午 9:50
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>这是一个页面</title>
+</head>
+
+<body>
+<h1 style="color: crimson">今天是星期六</h1>
+</body>
+</html>
